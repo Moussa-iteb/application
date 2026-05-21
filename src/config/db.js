d6ebd2a65ad3a2828ae31c01cb9d313cef1b8a99
Moussa-iteb <itebmoussa577@gmail.com>
2026-05-21 11:27:34 +0200

    fix env for railway

--- src/config/db.js
+++ src/config/db.js
@@ -4,18 +4,21 @@ module.exports = {
   development: {
     username: process.env.DB_USER || 'postgres',
     password: process.env.DB_PASSWORD || null,
-    database: process.env.DB_NAME || 'pfe',
+    database: process.env.DB_NAME || 'auth_db',
     host: process.env.DB_HOST || 'localhost',
     port: process.env.DB_PORT || 5432,
     dialect: 'postgres',
     logging: false
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
-    logging: false
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 };
