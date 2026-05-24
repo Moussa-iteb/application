fce9e154a6abc6034c5aacbbb4249aa01d8a394b
Moussa-iteb <itebmoussa577@gmail.com>
2026-05-24 14:58:08 +0200

    fix email service with nodemailer

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -1,10 +1,16 @@
-const { Resend } = require('resend');
+const nodemailer = require('nodemailer');
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  }
+});
 
 async function sendResetCode(toEmail, code) {
-  await resend.emails.send({
-    from: 'SmartRide <onboarding@resend.dev>',
+  await transporter.sendMail({
+    from: `SmartRide <${process.env.EMAIL_USER}>`,
     to: toEmail,
     subject: 'Password Reset Code',
     html: `
