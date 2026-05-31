bada7eff9c27f4efce4ac50319cbc9cbfdcbc15a
Moussa-iteb <itebmoussa577@gmail.com>
2026-05-31 23:33:59 +0200

    remove .env from git tracking

--- src/models/notification-log.model.js
+++ src/models/notification-log.model.js
@@ -1,14 +1,15 @@
+// models/notification-log.model.js
 const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
   return sequelize.define('NotificationLog', {
-    title:        { type: DataTypes.STRING,                              allowNull: false },
+    title:        { type: DataTypes.STRING,                   allowNull: false },
     body:         { type: DataTypes.TEXT },
-    target:       { type: DataTypes.ENUM('all', 'user'),                defaultValue: 'all' },
-    userId:       { type: DataTypes.INTEGER,                            allowNull: true },
-    successCount: { type: DataTypes.INTEGER,                            defaultValue: 0 },
-    failureCount: { type: DataTypes.INTEGER,                            defaultValue: 0 },
-    totalReached: { type: DataTypes.INTEGER,                            defaultValue: 0 },
+    target:       { type: DataTypes.ENUM('all', 'user'),      defaultValue: 'all' },
+    userId:       { type: DataTypes.INTEGER,                  allowNull: true },
+    successCount: { type: DataTypes.INTEGER,                  defaultValue: 0 },
+    failureCount: { type: DataTypes.INTEGER,                  defaultValue: 0 },
+    totalReached: { type: DataTypes.INTEGER,                  defaultValue: 0 },
   }, {
     tableName: 'NotificationLogs',
     timestamps: true,
